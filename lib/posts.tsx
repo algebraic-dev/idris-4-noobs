4d0901ef5b6aaebe982d43137d4744982497f3aa
Chiyoku <chiyokuoni@gmail.com>
2021-11-02 19:56:57 -0300

    feat: added post_utils to help in the fetching

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,17 +1,7 @@
 import path from 'path'
 import matter from 'gray-matter'
 import { promises as fs } from 'fs'
-
-interface PostData {
-  content: string
-  data: {
-    title: string
-    description: string
-  }
-}
-
-type PostFile = { type: 'File'; filename: string; data: PostData }
-type PostDir = { type: 'Dir'; name: string; posts: PostFile[] }
+import { PostData, PostDir, PostFile } from './post_utils'
 
 const readPost = async (file: string): Promise<PostData> => {
   const text = (await fs.readFile(file)).toString('utf-8')
@@ -56,46 +46,5 @@ const getPost = (name: string) => readPost(path.join(process.cwd(), name))
 
 // Generates path and the url form the three
 
-const genUrl = (str: string) => str.toLowerCase().replace(/ /g, '-')
-
-const genPathFile = (postFile: PostFile): [string, string] => [
-  postFile.filename,
-  postFile.filename + '.mdx',
-]
-
-const genPathDir = (postDir: PostDir): [string, string][] =>
-  postDir.posts.map(file => [
-    genUrl(postDir.name) + '/' + genPathFile(file)[0],
-    postDir.name + '/' + file.filename,
-  ])
-
-const genPath = (list: (PostDir | PostFile)[]): [string, string][] =>
-  list.map(a => (a.type == 'Dir' ? genPathDir(a) : [genPathFile(a)])).flat(1)
-
-// findURL from path
-
-function findData(
-  tree: (PostDir | PostFile)[],
-  path: string[]
-): PostData | null {
-  for (const node of tree) {
-    if (node.type == 'File' && path[0] == node.filename) return node.data
-    if (node.type == 'Dir' && path[0] == genUrl(node.name)) {
-      for (const leaf of node.posts) {
-        if (leaf.filename == path[1]) return leaf.data
-      }
-    }
-  }
-  return null
-}
-
-export {
-  readPosts,
-  readPost,
-  getResourceFolder,
-  getPost,
-  genPath,
-  findData,
-  genUrl,
-}
+export { readPosts, readPost, getResourceFolder, getPost }
 export type { PostDir, PostFile, PostData }
